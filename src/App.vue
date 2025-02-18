e337022d6604d16492c930025f79f4b7f79b722a
muhammadderic <muhammad.donny.e@gmail.com>
2025-02-18 11:08:25 +0700

    Create 'Scroll to Sections Strategy'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <script setup>
-import {  RouterView } from 'vue-router'
+// import { defineAsyncComponent } from 'vue';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
+import CarouselCompt from '@/components/CarouselCompt.vue';
+import NavBar from '@/components/NavBar.vue';
+
+// const CarouselCompt = defineAsyncComponent({
+//   loader: () => import('@/components/CarouselCompt.vue'),
+//   delay: 5000, // Show spinner only after 3 seconds
+// });
 </script>
 
 <template>
-  <RouterView />
+  <Suspense>
+    <template #default>
+      <div>
+      <NavBar/>
+      <CarouselCompt/>
+      </div>
+    </template>
+
+    <template #fallback>
+      <LoadingSpinner />
+    </template>
+  </Suspense>
 </template>
